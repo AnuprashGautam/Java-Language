6c99c1ea3c7af07827bfd9dc51b3efa758ce8918
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-06-05 10:59:42 +0530

    Renamed folder vid024 ->vid025.

---
+++ vid025/ImportantCodes.java
@@ -0,0 +1,19 @@
+public class ImportantCodes {
+    public static void main(String[] args) {
+        if (true)
+            System.out.println("true");
+
+        if (true)
+            ;
+
+        if (true)
+            ;;;;;
+
+        if (true) {
+            int x = 10;
+        }
+
+        // if(true)
+        //     int z=10;
+    }
+}
