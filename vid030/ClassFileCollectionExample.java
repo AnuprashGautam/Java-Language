0fd8dac1260124f125236788e4eb9461cd347653
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-06-07 13:00:08 +0530

    Adding the code files of video 30.

---
+++ vid030/ClassFileCollectionExample.java
@@ -0,0 +1,24 @@
+class A
+{
+    public static void main(String[] args) {
+        System.out.println("Main class A.");
+    }
+}
+
+class B
+{
+    public static void main(String[] args) {
+        System.out.println("Main class B.");
+    }
+}
+
+class C
+{
+    public static void main(String[] args) {
+        System.out.println("Main class C.");
+    }
+}
+
+class D 
+{
+}
---
+++ vid030/ImportStatementExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ImportStatementExample 
+{
+    public static void main(String[] args) 
+    {
+        // java.util.ArrayList l=new java.util.ArrayList();                   //We have to use fully qualified named everytime. It increases the code length.We can use "import statement" to overcome this problem.
+
+        ArrayList l=new ArrayList();                                           //Works as typing shortcut.
+    }
+}
