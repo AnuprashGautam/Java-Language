bfee23608683548e8efd1e308217e83473d73e33
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-06-08 20:26:10 +0530

    Adding the files and class file folder of the video 33. In which we discussed about package satement.

---
+++ vid033/PackageExample.java
@@ -0,0 +1,9 @@
+package newpackage.newmodule.submodule;            //Class file exist in the declared path in package statement.
+
+public class PackageExample
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Package Example.");
+    }
+}
