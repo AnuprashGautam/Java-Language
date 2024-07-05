350d3adcc0f857253debaeb2e9c8038056426a88
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-07-05 12:33:59 +0530

    Deleted file and restructured the vid041 folder.

--- vid041/InterfaceExample.java
+++
@@ -1,8 +0,0 @@
-interface InterfaceExample {
-    int x=10;                // Interface variable is public, sttaic and final. It should be initialized at the time of declaration.
-
-    void m1();               // Interface methods are public and abstract
-    void m2();
-
-    // Interface doesnot contain static block.
-}
