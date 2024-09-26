93a0a56078894d4f453a77b0e144e9adae6f7525
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-09-26 22:19:01 +0530

    video 14 got updated

--- 01. Language Fundamentals/vid014/StaticBlockAndMainMethod.java
+++ 01. Language Fundamentals/vid014/StaticBlockAndMainMethod.java
@@ -1,10 +1,10 @@
-public class StaticMembersAndMainMethod {
+public class StaticBlockAndMainMethod {
     static{
         System.out.println("Static block");
     }
-    public static void main(String[] args) {
-        System.out.println("Main Method");
-    }
+    // public static void main(String[] args) {
+    //     System.out.println("Main Method");
+    // }
 
     //1.7 version onwards it is imposiible to print anything without the main method. In the 1.7 and onward versions main method is checked first and then static members are checked.
 }
