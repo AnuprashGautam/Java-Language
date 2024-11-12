3c2e93da0682a431b1eed0d557ca8e11705997a0
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-11-12 14:55:55 +0530

    Added the video 38 codes.

--- 04. Declarations and Access Modifiers/vid037/FinalLocalVariable/FinalLocalVariableExample1.java
+++ 04. Declarations and Access Modifiers/vid037/FinalLocalVariable/FinalLocalVariableExample1.java
@@ -3,7 +3,7 @@ public class FinalLocalVariableExample1 {
         // For local variable JVM don't provide default value.
         // But if we are not using the uninitialised local variable, then we will not
         // get compile time error.
-        // Eventhoough loacl variable (uninitialised) is final and have not been used, even then we will not get compile time error.
+        // Eventhough local variable (uninitialised) is final and have not been used, even then we will not get compile time error.
         //Only applicable modifier for local variable is final.
 
         final int x;
