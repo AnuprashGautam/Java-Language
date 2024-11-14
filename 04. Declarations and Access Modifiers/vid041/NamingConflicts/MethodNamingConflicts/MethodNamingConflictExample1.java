0a90966c8983b9a0173f1137d23697e46ee0ea10
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-11-14 19:47:29 +0530

    Added new files.

--- 04. Declarations and Access Modifiers/vid041/NamingConflicts/MethodNamingConflicts/MethodNamingConflictExample1.java
+++ 04. Declarations and Access Modifiers/vid041/NamingConflicts/MethodNamingConflicts/MethodNamingConflictExample1.java
@@ -1,7 +1,3 @@
-package MethodNamingConflicts;
-import vid041.NamingConflicts.MethodNamingConflicts.Left;
-import vid041.NamingConflicts.MethodNamingConflicts.Right;
-
 interface Left {
     void m1();
 }
@@ -14,6 +10,16 @@ public class MethodNamingConflictExample1 implements Left,Right
 {
     public void m1(){
         System.out.println("One implementation for the both methods of both interfaces.");
+    }
+
+    public static void main(String[] args) {
+        Left l=new MethodNamingConflictExample1();
+
+        l.m1();
+
+        Right r=new MethodNamingConflictExample1();
+
+        r.m1();
     }
 }
 
--- 04. Declarations and Access Modifiers/vid041/NamingConflicts/MethodNamingConflicts/MethodNamingConflictExample2.java
+++ 04. Declarations and Access Modifiers/vid041/NamingConflicts/MethodNamingConflicts/MethodNamingConflictExample2.java
@@ -20,7 +20,15 @@ public class MethodNamingConflictExample2 implements Left, Right
         System.out.println("Implementation for the Right interface method:"+i);
     }
 
+    public static void main(String[] args) {
+        Left l=new MethodNamingConflictExample2();
+        l.m1();
+
+        Right r=new MethodNamingConflictExample2();
+        r.m1(10);
+    }
+
 }
 
 
-// If there is a case in which the methods of two cases have the disimilarity of onlu return type, then it is immpossible to provide the implementations to them with a same class.
+// If there is a case in which the methods of two cases have the disimilarity of only return type, then it is immpossible to provide the implementations to them with a same class.
