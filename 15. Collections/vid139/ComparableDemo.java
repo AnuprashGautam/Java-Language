0b14b82195e38c9cac0b60315b68496033d53552
Anuprash <anuprashgautam@hotmail.com>
2025-08-22 23:54:24 +0530

    Completed video 139.

---
+++ 15. Collections/vid139/ComparableDemo.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+public class ComparableDemo {
+    public static void main(String[] args) {
+        TreeSet t =new TreeSet();
+
+        t.add("K");
+        t.add("Z");
+        t.add("A");
+        t.add("A");
+
+        System.out.println(t);
+    }
+}
